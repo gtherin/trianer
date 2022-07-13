4ad8b864652ac62e6bc8d2bc6f30e394dcebfc38
guydegnol <gt@guydegnol.net>
2022-07-13 10:02:28 +0200

    Some changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,79 +2,113 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
+import datetime
 from datetime import time
+import trianer
+from streamlit_folium import folium_static
+import folium
 
 st.set_option("deprecation.showPyplotGlobalUse", False)
-st.sidebar.title("Athlete")
 
 
-poids = st.sidebar.slider(label="Poids (en kg)", min_value=50, max_value=100, value=80)
-# epreuve = st.sidebar.slider(label="Poids (en kg)", min_value=50, max_value=100, value=80)
-
-epreuve = st.sidebar.selectbox("Epreuve", ["Elsassman", "Deauville", "Bois"])
-longueur = st.sidebar.selectbox("Longueur", ["S", "M", "L"], index=2)
-
-# natation = st.sidebar.slider(label="Poids (en kg)", min_value=50, max_value=100, value=80)
-# natation = st.sidebar.selectbox("Longueur", ["S", "M", "L"])
+st.sidebar.markdown("""[website](https://trianer.guydegnol.net/)""")
 
 
-natation = st.sidebar.slider("Allure au 100m", value=time(2, 10), min_value=time(1, 30), max_value=time(3, 0))
-transition1 = st.sidebar.slider("Transition 1", value=time(5, 0), min_value=time(1, 0), max_value=time(10, 0))
-cyclisme = st.sidebar.slider("Vitesse en km", value=28, min_value=20, max_value=40)
-transition2 = st.sidebar.slider("Transition 2", value=time(5, 0), min_value=time(1, 0), max_value=time(10, 0))
-course = st.sidebar.slider("Allure au km", value=time(5, 0), min_value=time(3, 0), max_value=time(8, 0))
-
-st.write("Natation ", natation, "min/100m, cyclisme ", cyclisme, " km/h, course ", course, " min/km")
-
-st.title("Performance")
-
-POP_MIN, POP_MAX = st.sidebar.slider(
-    "Select the population range (Example: range of age is 0-100)", 0, 10000, (0, 1000)
-)
+if "sath_name" not in st.session_state:
+    st.session_state["sath_name"] = "Sylvia"
 
 
-pop_size = st.sidebar.slider(
-    label="Choose the Population size ('N') - Creates a population of size 'N' within the population range",
-    min_value=1000,
-    max_value=20000,
-    value=5000,
-    step=10,
-)
+def configure_athlete():
+    def update_athlete_name():
+        # athletes_names = ["Guillaume", "Cyrille", "Sylvia", "Vanina", "Nouveau"]
+        st.session_state.sath_name = st.session_state.ath_name
+
+    athletes_names = ["Guillaume", "Cyrille", "Sylvia", "Vanina"]
+
+    ath_name = st.selectbox("Athlete", athletes_names, key="sath_name")
+
+    # st.text_input(ath_name, value=ath_name, on_change=update_athlete_name)
+
+    st.write("Mon nom est ", ath_name)
+
+    poids = st.number_input("Poids (en kg)", min_value=50, max_value=100, value=80)
+    st.write("Mon poids est de", poids)
+
+    birthday = st.date_input("Anniversaire", datetime.date(1979, 11, 2))
+    st.write("Je suis né le ", birthday)
 
 
-@st.cache
-def generate_population():
-    np.random.seed(11)
-    population = np.random.randint(low=POP_MIN, high=POP_MAX, size=pop_size)
-    return population
+def configure_race():
+    epreuve = st.selectbox("Epreuve", ["Elsassman", "Deauville", "Bois", "Nouvelle"])
+    longueur = st.selectbox("Longueur", ["S", "M", "L"], index=2)
+    tri = trianer.Triathlon(epreuve=epreuve, longueur=longueur)
+    folium_static(tri.show_gpx_track())
+
+    input = st.file_uploader("")
+
+    if input is None:
+        st.write("Or use sample dataset to try the application")
+        sample = st.checkbox("Download sample data from GitHub")
+
+    st.button("Re-run")
 
 
-population = generate_population()
-st.sidebar.write(f"(Population mean, std): ({np.round(np.mean(population),2)}, {np.round(np.std(population),2)})")
+def configure_performance():
+    natation = st.select_slider(
+        "Allure pour la natation",
+        options=[f"{m+1}min{s:02.0f}s/100m" for m in range(1, 2) for s in np.linspace(0, 55, 12)],
+    )
+
+    transition1 = st.select_slider(
+        "Transition nat./cycl.",
+        options=[f"{m+1}min{s:02.0f}s" for m in range(1, 2) for s in np.linspace(0, 50, 6)],
+    )
+
+    cyclisme = st.number_input("Vitesse à plat en cyclisme", min_value=20, max_value=45, value=28)
+
+    transition2 = st.select_slider(
+        "Transition cycl./course",
+        options=[f"{m+1}min{s:02.0f}s" for m in range(0, 2) for s in np.linspace(0, 50, 6)],
+    )
+
+    course = st.select_slider(
+        "Vitesse en course à pieds",
+        options=[f"{m+1}min{s:02.0f}s/km" for m in range(1, 2) for s in np.linspace(0, 55, 12)],
+    )
 
 
-sample_size = st.sidebar.slider(label="Choose the sample size (n)", min_value=10, max_value=2000, value=100, step=10)
+def configure_physiologie():
 
-sample_number = st.sidebar.slider(
-    label="Choose the number of samples", min_value=10, max_value=2000, value=100, step=10
-)
+    with st.expander("Data format"):
+        st.write(
+            "The dataset can contain multiple columns but you will need to select a column to be used as dates and a second column containing the metric you wish to forecast. The columns will be renamed as **ds** and **y** to be compliant with Prophet. Even though we are using the default Pandas date parser, the ds (datestamp) column should be of a format expected by Pandas, ideally YYYY-MM-DD for a date or YYYY-MM-DD HH:MM:SS for a timestamp. The y column must be numeric."
+        )
+
+    natation = st.select_slider(
+        "Allure pour la natation",
+        options=[f"{m+1}min{s:02.0f}s/100m" for m in range(1, 2) for s in np.linspace(0, 55, 12)],
+    )
+
+    hydra = st.number_input("Hydratation", min_value=50, max_value=100, value=80)
+
+    st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+    st.write("My favorite color is", hydra)
 
 
-@st.cache
-def generate_samples():
-    np.random.seed(11)
-    sample_index = np.random.randint(low=0, high=len(population), size=(sample_number * sample_size))
-    sample = population[sample_index].reshape(sample_number, sample_size)
-    sample_means = np.mean(sample, axis=1)
-    return sample_means
+page_names_to_funcs = {
+    "Athlete": configure_athlete,
+    "Epreuve": configure_race,
+    "Performance": configure_performance,
+    "Physiologie": configure_physiologie,
+}
 
+demo_name = st.sidebar.radio("Configuration", page_names_to_funcs.keys())
+page_names_to_funcs[demo_name]()
 
-sample_means = generate_samples()
-
-import trianer
 
 # trianer.Triathlon(epreuve=epreuve, longueur=longueur).show_weather_forecasts()
 
+"""
 guillaume = trianer.Triathlon(
     epreuve=epreuve,
     longueur=longueur,
@@ -91,12 +125,12 @@ guillaume = trianer.Triathlon(
 )
 
 
-from streamlit_folium import folium_static
-import folium
-
 with st.echo():
     folium_static(guillaume.show_gpx_track())
+
+st.write("Natation ", natation, "min/100m, cyclisme ", cyclisme, " km/h, course ", course, " min/km")
 st.pyplot(guillaume.show_race_details())
 # st.pyplot(guillaume.show_race_details(xaxis = "itime"))
 st.pyplot(guillaume.show_nutrition())
-st.write(guillaume.show_roadmap())
+st.table(guillaume.show_roadmap())
+"""
