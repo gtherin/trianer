c7f9df7f2f5a61809c51b2cbe549f28fe809af66
guydegnol <gt@guydegnol.net>
2023-11-24 18:29:47 +0100

    pep8

--- trianer/race/gpx_formatter.py
+++ trianer/race/gpx_formatter.py
@@ -11,7 +11,9 @@ def get_default_datetime():
 class Point:
     @staticmethod
     def get_anchor_value(point, anchor):
-        return point[point.find(f"<{anchor}>") + len(f"<{anchor}>") : point.find(f"</{anchor}>")]
+        return point[
+            point.find(f"<{anchor}>") + len(f"<{anchor}>") : point.find(f"</{anchor}>")
+        ]
 
     @staticmethod
     def get_latitude(point):
@@ -46,14 +48,19 @@ class Point:
 
         point = self.npoint
         self.npoint = (
-            point[: point.find("<ele>") + len("<ele>")] + "%.2f" % altitude + point[point.find("</ele>") :]
+            point[: point.find("<ele>") + len("<ele>")]
+            + "%.2f" % altitude
+            + point[point.find("</ele>") :]
         )
 
     def delete_tag(self, tag):
         if not self.is_valid_gpx or tag not in self.npoint:
             return
         point = self.npoint
-        self.npoint = point[: point.find(tag)] + point[point.find(tag.replace("<", "</")) + len(tag) + 1 :]
+        self.npoint = (
+            point[: point.find(tag)]
+            + point[point.find(tag.replace("<", "</")) + len(tag) + 1 :]
+        )
 
     def __init__(self, point, splitter="<trkpt", remove_time=True, debug=False):
         self.is_valid_gpx = "lat=" in point
@@ -71,7 +78,6 @@ class Point:
         self.debug = debug
 
         if self.is_valid_gpx:
-
             self.point = splitter + point
             self.latitude = self.get_latitude(self.point)
             self.longitude = self.get_longitude(self.point)
@@ -129,7 +135,9 @@ class GpxFormatter:
         altitude_offset = 0
         if ctime >= filter["min_time"] and ctime <= filter["max_time"]:
             if type(filter["altitude_offset"]) == dict:
-                altitude_global_offset = filter["altitude_offset"]["max"] - filter["altitude_offset"]["min"]
+                altitude_global_offset = (
+                    filter["altitude_offset"]["max"] - filter["altitude_offset"]["min"]
+                )
 
                 altitude_offset = altitude_global_offset * (
                     get_diff_in_seconds(ctime, filter["min_time"]) / filter["diff_time"]
@@ -139,7 +147,10 @@ class GpxFormatter:
             else:
                 altitude_offset = filter["altitude_offset"]
 
-            print("%s: Altitude %s offset to %sm" % (self.filename, point.dtime.time(), int(altitude_offset)))
+            print(
+                "%s: Altitude %s offset to %sm"
+                % (self.filename, point.dtime.time(), int(altitude_offset))
+            )
             point.altitude += altitude_offset
         return point
 
@@ -171,12 +182,12 @@ class GpxFormatter:
 
         self.palt = point.altitude
 
-
         self.filecontent += point.get_formatted_point(debug=debug)
 
     @staticmethod
-    def clean_file(filename, filters, remove_time=True, debug=False, creator="GPS Track Editor"):
-
+    def clean_file(
+        filename, filters, remove_time=True, debug=False, creator="GPS Track Editor"
+    ):
         gpx_file = GpxFormatter(filename, creator=creator)
 
         if not gpx_file.exists():
@@ -184,25 +195,33 @@ class GpxFormatter:
             return
 
         for d, p in enumerate(gpx_file.xml):
-            gpx_file.update_filecontent(p, filters, remove_time=remove_time, debug=debug and d < 10)
+            gpx_file.update_filecontent(
+                p, filters, remove_time=remove_time, debug=debug and d < 10
+            )
 
         print(f"Denivelé total: d+={gpx_file.dup:.0f}m d-={-gpx_file.ddo:.0f}m")
         gpx_file.save()
 
     @staticmethod
     def clean_files(filename="gpx.json", filters=None, debug=False, remove_time=True):
-
         if filters is None:
             filters = json.load(open(filename, "r"))
 
         for filename, filters in filters.items():
-
             for filter in filters:
-                filter["min_time"] = datetime.datetime.strptime(filter["min_time"], "%H:%M:%S").time()
-                filter["max_time"] = datetime.datetime.strptime(filter["max_time"], "%H:%M:%S").time()
-                filter["diff_time"] = get_diff_in_seconds(filter["max_time"], filter["min_time"])
+                filter["min_time"] = datetime.datetime.strptime(
+                    filter["min_time"], "%H:%M:%S"
+                ).time()
+                filter["max_time"] = datetime.datetime.strptime(
+                    filter["max_time"], "%H:%M:%S"
+                ).time()
+                filter["diff_time"] = get_diff_in_seconds(
+                    filter["max_time"], filter["min_time"]
+                )
 
-            GpxFormatter.clean_file(filename, filters, debug=debug, remove_time=remove_time)
+            GpxFormatter.clean_file(
+                filename, filters, debug=debug, remove_time=remove_time
+            )
 
 
 if __name__ == "__main__":
